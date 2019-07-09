57755c7063b2c5edb96fc2b0a706517bf9a97b37
mac <mac@AlaaMacBookPro.local>
2019-07-09 15:39:06 +0200

    react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/layout/Navbar';
+import index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import { Provider } from './context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+const Context = React.createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH_TRACKS':
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: 'Search Results',
+      };
+    default:
+      return state;
+  }
+};
+export class Provider extends React.Component {
+  state = {
+    track_list: [],
+    heading: 'Top ten Tracks',
+    dispatch: action => this.setState(state => reducer(state, action)),
+  };
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=US&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`,
+      )
+      .then(res => {
+        this.setState({ track_list: res.data.message.body });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    return <Context.Provider value={this.state.track_list}>{this.props.children}</Context.Provider>;
+  }
+}
+export const Consumer = Context.Consumer;
